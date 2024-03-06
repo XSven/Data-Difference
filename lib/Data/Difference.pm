eae44e049c55ca3670a3113f0d6efce6dd8b19e3
Sven Willenbuecher <sven.willenbuecher@kuehne-nagel.com>
2024-03-06 09:07:25 +0100

    fix things to make dzil happy

--- lib/Data/Difference.pm
+++ lib/Data/Difference.pm
@@ -1,54 +1,51 @@
+#<<<
+use strict; use warnings;
+#>>>
+
 package Data::Difference;
+# ABSTRACT: Compare simple hierarchical data
 
-use strict;
-use warnings;
-use base 'Exporter';
+use Exporter qw( import );
 
-our @EXPORT_OK = qw(data_diff);
+our @EXPORT_OK = qw( data_diff );
 
 sub data_diff {
-  my ($a, $b) = @_;
+  my ( $a, $b ) = @_;
 
-  if (ref($a)) {
-    if (my $sub = __PACKAGE__->can("_diff_" . ref($a))) {
-      return $sub->($a, $b);
+  if ( ref( $a ) ) {
+    if ( my $sub = __PACKAGE__->can( '_diff_' . ref( $a ) ) ) {
+      return $sub->( $a, $b );
+    } else {
+      return { path => [], a => $a, b => $b };
     }
-    else {
-      return {path => [], a => $a, b => $b};
-    }
-  }
-  elsif (defined $a ? defined $b ? $a ne $b : 1 : 0) {
-    return {path => [], a => $a, b => $b};
+  } elsif ( defined $a ? defined $b ? $a ne $b : 1 : 0 ) {
+    return { path => [], a => $a, b => $b };
   }
 
   return;
 }
 
 sub _diff_HASH {
-  my ($a, $b, @path) = @_;
+  my ( $a, $b, @path ) = @_;
 
-  return {path => \@path, a => $a, b => $b} unless ref($a) eq ref($b);
+  return { path => \@path, a => $a, b => $b } unless ref( $a ) eq ref( $b );
 
   my @diff;
   my %k;
-  @k{keys %$a, keys %$b} = ();
-  foreach my $k (sort keys %k) {
-    if (!exists $a->{$k}) {
-      push @diff, {path => [@path, $k], b => $b->{$k}};
-    }
-    elsif (!exists $b->{$k}) {
-      push @diff, {path => [@path, $k], a => $a->{$k}};
-    }
-    elsif (ref($a->{$k})) {
-      if (my $sub = __PACKAGE__->can("_diff_" . ref($a->{$k}))) {
-        push @diff, $sub->($a->{$k}, $b->{$k}, @path, $k);
+  @k{ keys %$a, keys %$b } = ();
+  foreach my $k ( sort keys %k ) {
+    if ( !exists $a->{ $k } ) {
+      push @diff, { path => [ @path, $k ], b => $b->{ $k } };
+    } elsif ( !exists $b->{ $k } ) {
+      push @diff, { path => [ @path, $k ], a => $a->{ $k } };
+    } elsif ( ref( $a->{ $k } ) ) {
+      if ( my $sub = __PACKAGE__->can( '_diff_' . ref( $a->{ $k } ) ) ) {
+        push @diff, $sub->( $a->{ $k }, $b->{ $k }, @path, $k );
+      } else {
+        push @diff, { path => [ @path, $k ], a => $a->{ $k }, b => $b->{ $k } };
       }
-      else {
-        push @diff, {path => [@path, $k], a => $a->{$k}, b => $b->{$k}};
-      }
-    }
-    elsif (defined $a->{$k} ? defined $b->{$k} ? $b->{$k} ne $a->{$k} : 1 : defined $b->{$k}) {
-      push @diff, {path => [@path, $k], a => $a->{$k}, b => $b->{$k}};
+    } elsif ( defined $a->{ $k } ? defined $b->{ $k } ? $b->{ $k } ne $a->{ $k } : 1 : defined $b->{ $k } ) {
+      push @diff, { path => [ @path, $k ], a => $a->{ $k }, b => $b->{ $k } };
     }
   }
 
@@ -56,29 +53,25 @@ sub _diff_HASH {
 }
 
 sub _diff_ARRAY {
-  my ($a, $b, @path) = @_;
-  return {path => \@path, a => $a, b => $b} unless ref($a) eq ref($b);
+  my ( $a, $b, @path ) = @_;
+  return { path => \@path, a => $a, b => $b } unless ref( $a ) eq ref( $b );
 
   my @diff;
   my $n = $#$a > $#$b ? $#$a : $#$b;
 
-  foreach my $i (0 .. $n) {
-    if ($i > $#$a) {
-      push @diff, {path => [@path, $i], b => $b->[$i]};
-    }
-    elsif ($i > $#$b) {
-      push @diff, {path => [@path, $i], a => $a->[$i]};
-    }
-    elsif (ref($a->[$i])) {
-      if (my $sub = __PACKAGE__->can("_diff_" . ref($a->[$i]))) {
-        push @diff, $sub->($a->[$i], $b->[$i], @path, $i);
+  foreach my $i ( 0 .. $n ) {
+    if ( $i > $#$a ) {
+      push @diff, { path => [ @path, $i ], b => $b->[ $i ] };
+    } elsif ( $i > $#$b ) {
+      push @diff, { path => [ @path, $i ], a => $a->[ $i ] };
+    } elsif ( ref( $a->[ $i ] ) ) {
+      if ( my $sub = __PACKAGE__->can( '_diff_' . ref( $a->[ $i ] ) ) ) {
+        push @diff, $sub->( $a->[ $i ], $b->[ $i ], @path, $i );
+      } else {
+        push @diff, { path => [ @path, $i ], a => $a->[ $i ], b => $b->[ $i ] };
       }
-      else {
-        push @diff, {path => [@path, $i], a => $a->[$i], b => $b->[$i]};
-      }
-    }
-    elsif (defined $a->[$i] ? defined $b->[$i] ? $b->[$i] ne $a->[$i] : 1 : defined $b->[$i]) {
-      push @diff, {path => [@path, $i], a => $a->[$i], b => $b->[$i]};
+    } elsif ( defined $a->[ $i ] ? defined $b->[ $i ] ? $b->[ $i ] ne $a->[ $i ] : 1 : defined $b->[ $i ] ) {
+      push @diff, { path => [ @path, $i ], a => $a->[ $i ], b => $b->[ $i ] };
     }
   }
 
@@ -95,37 +88,38 @@ Data::Difference - Compare simple hierarchical data
 
 =head1 SYNOPSYS
 
-  use Data::Difference qw(data_diff);
+  use Data::Difference qw( data_diff );
   use Data::Dumper;
 
-  my %from = (Q => 1, W => 2, E => 3, X => [1,2,3], Y=> [5,6]);
-  my %to = (W => 4, E => 3, R => 5, => X => [1,2], Y => [5,7,9]);
-  my @diff = data_diff(\%from, \%to);
+  my %from = ( Q => 1, W => 2, E => 3, X => [ 1, 2, 3 ], Y=> [ 5, 6 ] );
+  my %to = ( W => 4, E => 3, R => 5, => X => [ 1, 2 ], Y => [ 5, 7, 9 ] );
+  my @diff = data_diff( \%from, \%to );
 
   @diff = (
-    # value $a->{Q} was deleted
-    { 'a'    => 1, 'path' => ['Q'] },
+    # value $a->{ Q } was deleted
+    { a    => 1, path => [ 'Q' ] },
 
-    # value $b->{R} was added
-    { 'b'    => 5, 'path' => ['R'] },
+    # value $b->{ R } was added
+    { b    => 5, path => [ 'R' ] },
 
-    # value $a->{W} changed
-    { 'a'    => 2, 'b'    => 4, 'path' => ['W'] },
+    # value $a->{ W } changed
+    { a    => 2, b    => 4, path => [ 'W' ] },
 
-    # value $a->{X}[2] was deleted
-    { 'a'    => 3, 'path' => ['X', 2] },
+    # value $a->{ X }[ 2 ] was deleted
+    { a    => 3, path => [ 'X', 2 ] },
 
-    # value $a->{Y}[1] was changed
-    { 'a'    => 6, 'b'    => 7, 'path' => ['Y', 1] },
+    # value $a->{ Y }[ 1 ] was changed
+    { a    => 6, b    => 7, path => [ 'Y', 1 ] },
 
-    # value $b->{Y}[2] was added
-    { 'b'    => 9, 'path' => ['Y', 2] },
+    # value $b->{ Y }[ 2 ] was added
+    { b    => 9, path => [ 'Y', 2 ] },
   );
 
 =head1 DESCRIPTION
 
-C<Data::Difference> will compare simple data structures returning a list of details about what was
-added, removed or changed. It will currently handle SCALARs, HASH references and ARRAY references.
+C<Data::Difference> will compare simple data structures returning a list of
+details about what was added, removed or changed. It will currently handle
+SCALARs, HASH references and ARRAY references.
 
 Each change is returned as a hash with the following element.
 
@@ -133,18 +127,21 @@ Each change is returned as a hash with the following element.
 
 =item path
 
-path will be an ARRAY reference containing the hierarchical path to the value, each element in the array
-will be either the key of a hash or the index on an array
+path will be an ARRAY reference containing the hierarchical path to the value,
+each element in the array will be either the key of a hash or the index on an
+array
 
 =item a
 
-If it exists it will contain the value from the first argument passed to C<data_diff>. If it
-does not exist then this element did not exist in the first argument.
+If it exists it will contain the value from the first argument passed to
+C<data_diff>. If it does not exist then this element did not exist in the first
+argument.
 
 =item b
 
-If it exists it will contain the value from the second argument passed to C<data_diff>. If it
-does not exist then this element did not exist in the second argument.
+If it exists it will contain the value from the second argument passed to
+C<data_diff>. If it does not exist then this element did not exist in the
+second argument.
 
 =back
 
--- t/diff.t
+++ t/diff.t
@@ -10,43 +10,54 @@ BEGIN { use_ok( 'Data::Difference', 'data_diff' ) or BAIL_OUT 'Cannot load modul
 my @tests = (
   { from => undef, to => undef, changes => [], name => 'no change (from and to are undefined)' },
   {
+    from    => [ 1, 2, 3 ],
+    to      => { W => 4, E => 3, R => 5 },
+    changes => [ { path => [], a => [ 1, 2, 3 ], b => { W => 4, E => 3, R => 5 } } ],
+    name    => 'compare incompatible reference types'
+  },
+  {
     from    => { D => { VERTICAL => 'down', HORIZONTAL => 'right' } },
     to      => { D => { VERTICAL => 'down', HORIZONTAL => 'right', FOO => 42 } },
     changes => [ { path => [ 'D', 'FOO' ], b => 42 } ],
-    name    => 'value $b->{D}{FOO} was added'
+    name    => 'value $b->{ D }{ FOO } was added'
   },
   { from => 1, to => 2, changes => [ { path => [], a => 1, b => 2 } ], name => 'value $a was changed' },
   {
     from    => [ 1, 2, 3 ],
     to      => [ 1, 2 ],
     changes => [ { path => [ 2 ], a => 3 } ],
-    name    => 'value $a->[2] was deleted'
+    name    => 'value $a->[ 2 ] was deleted'
   },
-  { from => [ 1, 2 ], to => [ 1, 2, 3 ], changes => [ { path => [ 2 ], b => 3 } ], name => 'value $b->[2] was added' },
+  {
+    from    => [ 1, 2 ],
+    to      => [ 1, 2, 3 ],
+    changes => [ { path => [ 2 ], b => 3 } ],
+    name    => 'value $b->[ 2 ] was added'
+  },
   {
     from    => { Q => 1, W => 2, E => 3 },
     to      => { W => 4, E => 3, R => 5 },
     changes => [ { path => [ 'Q' ], a => 1 }, { path => [ 'R' ], b => 5 }, { path => [ 'W' ], a => 2, b => 4 }, ],
-    name    => 'value $a->{Q} was deleted, value $b->{R} was added, value $a->{W} was changed'
+    name    => 'value $a->{ Q } was deleted, value $b->{ R } was added, value $a->{ W } was changed'
   },
   {
     from    => { X => [ 1, 2, 3 ], Y => [ 5, 6 ] },
     to      => { X => [ 1, 2 ], Y => [ 5, 7, 9 ] },
     changes =>
       [ { path => [ 'X', 2 ], a => 3 }, { path => [ 'Y', 1 ], a => 6, b => 7 }, { path => [ 'Y', 2 ], b => 9 }, ],
-    name => 'value $a->{X}[2] was deleted, value $a->{Y}[1] was changed, value $b->{Y}[2] was added'
+    name => 'value $a->{ X }[ 2 ] was deleted, value $a->{ Y }[ 1 ] was changed, value $b->{ Y }[ 2 ] was added'
   },
   {
     from    => { Z => 'foo' },
     to      => { Z => undef },
     changes => [ { path => [ 'Z' ], a => 'foo', b => undef } ],
-    name    => 'value $a->{Z} was changed to undef (rt.cpan.org #109262)'
+    name    => 'value $a->{ Z } was changed to undef (rt.cpan.org #109262)'
   },
   {
     from    => { Z => undef },
     to      => { Z => 'foo' },
     changes => [ { path => [ 'Z' ], a => undef, b => 'foo' } ],
-    name    => 'value $a->{Z} was changed from undef to defined value (rt.cpan.org #109262)'
+    name    => 'value $a->{ Z } was changed from undef to defined value (rt.cpan.org #109262)'
   },
 );
 
